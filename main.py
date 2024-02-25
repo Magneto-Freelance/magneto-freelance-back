3a6a1eb9d7a0fa3a9d2fcc8b2c06b63156f08dc2
Sara Pineda <spinedal@eafit.edu.co>
2024-02-24 21:19:37 -0500

    rutas get y post para empresas

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ MONGO_URL = os.environ.get("MONGO_URL", "mongodb://localhost:27017")
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
 db = client.get_database("Magneto-Freelance")
 postulant_collection = db.get_collection("Postulant")
+company_collection = db.get_collection("Company")
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
 
@@ -19,7 +20,7 @@ PyObjectId = Annotated[str, BeforeValidator(str)]
 class Postulant(BaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
     name: str = Field(...)
-    lastName: str = Field(...)
+    password: str = Field(...)
     cellphoneNumber: str = Field(...)
     email: str = Field(...)
     occupation: str = Field(...)
@@ -35,6 +36,7 @@ class PostulantCollection(BaseModel):
 class Company(BaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
     name: str = Field(...)
+    email: str = Field(...)
     description: str = Field(...)
     ubication: str = Field(...)
     type: str = Field(...)
@@ -63,7 +65,7 @@ app.add_middleware(
 
 #definicion de la ruta para hacer el create
 @app.post(
-    "/postulants/",
+    "/postulants",
     response_description="Add new Postulant",
     response_model=Postulant,
     status_code=status.HTTP_201_CREATED,
@@ -88,3 +90,31 @@ async def create_postulant(postulant: Postulant = Body(...)):
 )
 async def get_postulant():
     return PostulantCollection(postulants=await postulant_collection.find().to_list(1000))
+
+
+@app.post(
+    "/companies",
+    response_description="Add new Company",
+    response_model=Company,
+    status_code=status.HTTP_201_CREATED,
+    response_model_by_alias=False,
+)
+async def create_company(company: Company = Body(...)):
+    new_company = await company_collection.insert_one(
+        company.model_dump(by_alias=True, exclude=["id"])
+    )
+    created_company = await company_collection.find_one(
+        {"_id": new_company.inserted_id}
+    )
+    return created_company
+
+
+@app.get(
+    "/company",
+    response_description="Get companies",
+    response_model=List[Company], 
+    status_code=status.HTTP_200_OK,
+    response_model_by_alias=False,
+)
+async def get_company():
+    return await company_collection.find().to_list(1000)
